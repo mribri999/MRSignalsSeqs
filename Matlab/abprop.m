671f96bea19d552d2443900843c8a5a0458d98d4
Brian Hargreaves <brian@tofino.stanford.edu>
2017-09-14 14:05:55 -0700

    Fixed steady-state if not unique, cleaned up a bit.

--- Matlab/abprop.m
+++ Matlab/abprop.m
@@ -4,7 +4,10 @@
 %	Function 'propagates' matrices A and B to give overall A,B
 %	such that m' = A*m+B.  The Ai,Bi are applied in order, ie A1,B1
 %	are the first applied to m, then A2,B2 and so on.
-%	If mss is provided, the steady-state is calculated.
+%
+%	If mss is provided, the steady-state is calculated - in which case
+%	the A1,B1 list should start right after the point where it is desired
+%	to calculate mss.
 %
 %	If an Ai is 3x4, then it is assumed to be [Ai Bi]
 %
@@ -14,6 +17,7 @@
 function [A,B,mss] = abprop(varargin)
 
 A=eye(3); B=[0;0;0];		% Start with identity
+mss = [];			% Default
 
 argcount=1;
 while (argcount <= nargin)
@@ -34,27 +38,31 @@ while (argcount <= nargin)
     end;
   end;
 
-  if (size(Ai) == [3 4])	% If 3x4, then of form [Ai Bi]
+  if (size(Ai) == [3 4])	% -- If 3x4, then of form [Ai Bi]
     Bi = Ai(:,4);
     Ai = Ai(:,1:3);
   end;
 
-  % If Ai is not 3x4 or 3x3 then force an error.
+  % -- If Ai is not 3x4 or 3x3 then force an error.
   if (size(Ai,1)~=3) || (size(Ai,2)~=3)	
     tt = sprintf('Argument %d should be 3x3 or 3x4',2*count-1);
     error(tt); 
   end;
 
-  disp('Propagating...');
-  disp ([ Ai Bi]); 
+  %disp('Propagating...');	% -- Display for testing
+  %disp ([ Ai Bi]); 
 
   A = Ai*A;
   B = Ai*B+Bi;
 
 end;  
 
-if (nargout>2) 
-  mss = inv(eye(3)-A)*B;
+if (nargout>2) 			% -- Try to calculate Steady-State Mag.
+  if (rank(eye(3)-A)<3)
+    warning('No Unique Steady-State - mss will be []');
+  else
+    mss = inv(eye(3)-A)*B;
+  end;
 end;
 
 
