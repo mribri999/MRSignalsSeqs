40643ef7d18f7800c4cb6f65349857a796fb4d5f
Brian Hargreaves <bah@stanford.edu>
2020-03-26 19:06:18 -0700

    fixed axis label.

--- Matlab/plotm.m
+++ Matlab/plotm.m
@@ -1,8 +1,19 @@
 function plotm(M)
 %function plotm(M)
-%	Show spins on a plot with 3D, top view, left, right.
+%	Show spins on a plot with 3D, Mx-My, Mx-Mz, My-Mz views.
 %
-if (nargin < 1) M = [0.8,0,0.2].'; end;
+%	INPUT:
+%		M = 3xN magnetization to plot
+%		scale = (optional) scale for plots
+%
+%	OUTPUT:
+%		plot on current figure.
+
+
+if (nargin < 1) 
+  disp('No argmenents passed to plotm - using defaults')	
+  M = [0.8,0,0.2].'; 
+end;
 
 clf;
 subplot(2,2,1);
@@ -12,7 +23,7 @@ title('3D View')
 subplot(2,2,2);
 showspins(M);
 view(0,90);
-tt = sprintf('Mx - Mz'); title(tt);
+tt = sprintf('Mx - My'); title(tt);
 
 subplot(2,2,3);
 showspins(M);
