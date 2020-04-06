5a84945339c2ff1f4015134c40bc57d85e650b7d
Brian Hargreaves <bah@stanford.edu>
2020-04-06 13:56:24 -0700

    minor

--- Matlab/lectures/lec2_01.m
+++ Matlab/lectures/lec2_01.m
@@ -16,8 +16,15 @@ dat3=dat; dat3(:,1:64)=0; dat3(:,193:end)=0;   % Zero outer k-space
 im3=ft(dat3);
 
 % -- Display all images
-dispim(cat(2,im1,im2,im3)); 
-title('Images for a, b and c');
-axis equal;
-axis off;
+figure(1);
+dispim(im1); title('Basic Image'); axis off;
+
+figure(2); 
+dispim(im2);
+title('Image with Every 2nd vertical (S/I) k-space Line'); axis off;
+
+figure(3);
+dispim(im3);
+title('Image with Outer k-space set to 0'); axis off;
+
 
