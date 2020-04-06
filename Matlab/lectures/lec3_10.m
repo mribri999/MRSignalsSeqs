030e2cc8972417503316cee4f3be81c8bcd4a6dc
Brian Hargreaves <bah@stanford.edu>
2020-04-06 11:41:14 -0700

    minor.

--- Matlab/lectures/lec3_10.m
+++ Matlab/lectures/lec3_10.m
@@ -2,10 +2,13 @@
 %
 % Simulate RF with gradient twist
 
+TR = 0.01;		% s
+T1 = 1;			% s
+T2 = 0.2;		% s
 % -- Initialize
 z = [-1:0.05:1];                % Z locations
 Mc = [0*z; 0*z; ones(size(z))];  % Equilibrium
-[A,B] = relax(0.01,1,0.2);	% Relaxation over TR
+[A,B] = relax(TR,T1,T2);	% Relaxation over TR
 B = B*ones(size(z));		% Expand B vector 
 
 Rex = yrot(-30);		% 30 degree excitation
@@ -14,7 +17,7 @@ Rex = T*Rex*inv(T);		% R in complex-m representation
 
 % -- Phase Twist
 ph = exp(pi*i*z);		% One cycle of phase.
-phmult = [ph; conj(ph); 0*ph];  % 3xN phase Multiplier
+phmult = [ph; conj(ph); ones(size(ph))];  % 3xN phase Multiplier
 
 % -- Simulate to Steady State
 figure(1);
@@ -23,11 +26,11 @@ for k=1:200			% Steady state so just start after RF
   Mc = A*Mc+B;			% Relaxation				
   Mc = phmult.*Mc;		% Gradient twist
   Mc = Rex * Mc;		% Excitation
-  plotm(mc2mr(Mc));
-  drawnow;
+  %plotm(real(mc2mr(Mc))); 
+  %drawnow; pause;
 end;
 
-plotm(mc2mr(Mc));
+plotm(mc2mr(Mc),0.5);
  
 
 
