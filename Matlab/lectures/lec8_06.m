cf37d5c3a7575053ec5a72b680113dc93e69e55c
Brian Hargreaves <bah@stanford.edu>
2020-04-29 08:45:58 -0700

    minor

--- Matlab/lectures/lec8_06.m
+++ Matlab/lectures/lec8_06.m
@@ -4,7 +4,11 @@
 % Hyperecho example.
 
 % -- These lines setup a short hyperecho sequence:
-flips = [1 1.3 1.2 1.3] .* exp(pi*i*[0.62 -.28 .28 .25]);
+flips = [1 1.3 1.2 1.3] .* exp(pi*i*[0.62 -.28 .28 .25]);	% Random
+%flips = [2 1 1 1 1 1];		% Just reduced (~60 degree) train
+
+
+
 flips=[flips pi -fliplr(conj(flips))];         % Hyperecho refocusing
 
 clf;
