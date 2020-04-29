f7efc9d0f0698ed84e3d9ff075b2c964bcb2aead
Brian Hargreaves <bah@stanford.edu>
2020-04-29 08:22:47 -0700

    minor

--- Matlab/lectures/lec8_05.m
+++ Matlab/lectures/lec8_05.m
@@ -8,7 +8,7 @@ flips = pi/180*[120 60*ones(1,23)];
 set(0,'defaultAxesFontSize',14);	% Default font sizes
 set(0, 'DefaultLineLineWidth', 2);	% Default line width
 
-s = epg_cpmg(flips(k)*pi/180);	
+s = epg_cpmg(flips);
 
 
 
---
+++ Matlab/lectures/lec8_06.m
@@ -0,0 +1,16 @@
+
+% Lecture 8, Example 06
+% 
+% Hyperecho example.
+
+% -- These lines setup a short hyperecho sequence:
+flips = [1 1.3 1.2 1.3] .* exp(pi*i*[0.62 -.28 .28 .25]);
+flips=[flips pi -fliplr(conj(flips))];         % Hyperecho refocusing
+
+clf;
+set(0,'defaultAxesFontSize',14);	% Default font sizes
+set(0, 'DefaultLineLineWidth', 2);	% Default line width
+s = epg_cpmg(flips);	
+
+
+
