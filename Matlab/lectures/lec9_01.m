c521ccd28021072e62e537937da78739fa67a68e
Brian Hargreaves <bah@stanford.edu>
2020-05-04 07:38:51 -0700

    minor

---
+++ Matlab/lectures/lec9_01.m
@@ -0,0 +1,28 @@
+
+% Lecture 9, Example 01
+% 
+% Balanced SSFP signal plot
+
+TE = 0;	% ms
+TR = 5;		% ms
+flip = 60;	% degrees
+T1 = 1000;	% ms
+T2 = 200;	% ms
+rfdphase=180;    % degrees per TR
+
+tt=sprintf('TR/TE = %d/%d ms, Flip=%d deg, T1/T2=%d/%d ms',TR,TE,flip,T1,T2);
+disp(tt);
+
+df = -200:200;
+for k=1:length(df);
+  s(k) = bssfp(flip,TR,TE,T1,T2,df(k)+1000/TR*(rfdphase/360));
+end;
+
+
+set(0,'defaultAxesFontSize',14);	% Default font sizes
+set(0, 'DefaultLineLineWidth', 2);	% Default line width
+magphase(df,s);
+subplot(2,1,1); title('bSSFP signal vs Frequency (Hz)');
+subplot(2,1,2); xlabel('Frequency (Hz)');
+
+
