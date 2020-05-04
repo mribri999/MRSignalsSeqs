1ec1c044ec88c1409cde7bfd82b550c0534f90e6
Brian Hargreaves <bah@stanford.edu>
2020-05-04 07:57:12 -0700

    minor

--- Matlab/lectures/lec9_01.m
+++ Matlab/lectures/lec9_01.m
@@ -1,28 +1,44 @@
-
 % Lecture 9, Example 01
-% 
+%
 % Balanced SSFP signal plot
 
-TR = 5;		% ms
-TE = 0;		% ms
-flip = 60;	% degrees
-T1 = 1000;	% ms
-T2 = 200;	% ms
+TR = 5;         % ms
+TE = TR/2; % ms
+flip = 30;      % degrees
+T1 = 1000;      % ms
+T2 = 200;       % ms
 rfdphase=180;    % degrees per TR
+flips = [60,90,30,10,5,1];    % degrees
+fliplist = [1,4];             % Just do one flip angle to start
+labels = {'90','60','30','10','5','1'};
 
 tt=sprintf('TR/TE = %d/%d ms, Flip=%d deg, T1/T2=%d/%d ms',TR,TE,flip,T1,T2);
 disp(tt);
 
 df = -200:200;
-for k=1:length(df);
-  s(k) = bssfp(flip,TR,TE,T1,T2,df(k)+1000/TR*(rfdphase/360));
+s = zeros(length(df),length(flips(fliplist)));
+
+for f=1:length(flips(fliplist))
+  for k=1:length(df);
+    s(k,f) = bssfp(flips(fliplist(f)),TR,TE,T1,T2,df(k)+1000/TR*(rfdphase/360));
+  end;
 end;
 
+set(0,'defaultAxesFontSize',14);        % Default font sizes
+set(0, 'DefaultLineLineWidth', 2);      % Default line width
+figure(1);
+subplot(2,1,1);
+plot(df,abs(s)); 
+lplot('Frequency(Hz)','Signal Magnitude','bSSFP Signal vs Frequency (Hz)');
+legend(labels{fliplist});
 
-set(0,'defaultAxesFontSize',14);	% Default font sizes
-set(0, 'DefaultLineLineWidth', 2);	% Default line width
-magphase(df,s);
-subplot(2,1,1); title('bSSFP signal vs Frequency (Hz)');
-subplot(2,1,2); xlabel('Frequency (Hz)');
+subplot(2,1,2);
+plot(df,angle(s)/pi); 
+lplot('Frequency(Hz)','Signal Phase/\pi','bSSFP Signal vs Frequency (Hz)');
+legend(labels{fliplist});
 
+figure(2);
+plot(real(s),imag(s),'.');
+lplot('M_x','M_y','Signal in Mx-My Plane');
+axis equal;
 
